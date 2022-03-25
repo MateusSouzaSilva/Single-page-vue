7665327bcd8afe7fa94b36a15d47e6d532055fbc
Mateus Souza <mateus_souza11@hotmail.com.br>
2022-03-25 10:28:10 -0300

    base projeto

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <img src="https://i0.wp.com/www.portaldodog.com.br/cachorros/wp-content/uploads/2021/03/visa%CC%83o-do-cachorro-2.jpeg?resize=626%2C626&ssl=1" alt="cachorro">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        titulo: "PetShop"
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
